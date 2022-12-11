9edac1ecabe26423dae893fb2ff7e2f397b32e99
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-11 22:46:11 +0100

    Add findByLogin to user repository

--- src/main/java/com/example/library/repository/UserRepository.java
+++ src/main/java/com/example/library/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.library.repository;
 
 import com.example.library.entity.User;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+	Optional<User> findByLogin(String login);
 }
