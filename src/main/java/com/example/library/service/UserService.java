c6e625304f370c158e9f1f42c2d2d502b71c8287
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-11 22:49:06 +0100

    Implement find user by login

--- src/main/java/com/example/library/service/UserService.java
+++ src/main/java/com/example/library/service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.library.service;
 
 import com.example.library.entity.User;
+import java.util.Optional;
 
 public interface UserService {
+
+	Optional<User> findUserByLogin(String login);
 
 	User saveUser(User user);
 }
--- src/main/java/com/example/library/service/UserServiceImpl.java
+++ src/main/java/com/example/library/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.library.service;
 
 import com.example.library.entity.User;
 import com.example.library.repository.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+
+	@Override
+	public Optional<User> findUserByLogin(String login) {
+		return userRepository.findByLogin(login);
+	}
 
 	@Override
 	public User saveUser(User user) {
